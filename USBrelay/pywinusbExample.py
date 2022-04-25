d0931fea44e9f69761c399fee144dcf694ea1530
flambus <ilja.gavrylov@gmail.com>
2022-04-25 12:44:30 +0200

    25-04

---
+++ USBrelay/pywinusbExample.py
@@ -0,0 +1,26 @@
+from pywinusb import hid
+
+filter = hid.HidDeviceFilter(vendor_id = 0x16c0, product_id = 0x05df)
+hid_device = filter.get_devices()
+device = hid_device[0]
+device.open()
+print(hid_device)
+
+
+# target_usage = hid.get_full_usage_id(0x00, 0x3f)
+# device.set_raw_data_handler(sample_handler)
+# print(target_usage)
+#
+#
+report = device.find_output_reports()
+
+print(report)
+print(report[0])
+#
+# buffer = [0xFF]*64
+# buffer[0] = 63
+#
+# print(buffer)
+#
+# report[0].set_raw_data(buffer)
+# report[0].send()
